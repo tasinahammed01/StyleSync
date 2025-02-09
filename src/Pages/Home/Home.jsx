4506e7b56ce8836d401145c914d6b4f43ccbb31a
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-02-09 22:16:18 +0600

    beauty product section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import FeaturedProduct from '../../Components/FeaturedProduct/FeaturedProduct';
+import BeautyProducts from '../../Components/BeautyProducts/BeautyProducts';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <FeaturedProduct></FeaturedProduct>
+            <BeautyProducts></BeautyProducts>
         </div>
     );
 };
