659137c9157981bfbea7ad356e0f9f17c3e1b904
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-02-09 20:18:03 +0600

    header and hero seciotn complete

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Banner = () => {
+  return (
+    <div className="relative">
+      <img
+        className="w-full lg:h-auto md:h-[35vh] h-[50vh] object-cover"
+        src="https://i.postimg.cc/bJdGtVgk/Beige-Brown-Minimalist-Jewelry-Brand-Fashion-Photo-Collage-Facebook-Cover.jpg"
+        alt="Fashion Banner"
+      />
+      <div className="absolute lg:top-1/3 md:top-16 top-1/4 left-6 right-6 md:left-16 md:right-16 lg:left-32 lg:right-32 w-[90%] md:w-[70%] lg:w-[40%] space-y-6  md:space-y-6">
+        <h1 className="text-2xl  md:text-5xl lg:text-6xl font-bold text-white leading-tight">
+          Wear Confidence, Define Elegance
+        </h1>
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl text-white">
+          Unleash your signature style with exclusive fashion, statement
+          accessories, and luxury watches—crafted for those who dare to stand
+          out.
+        </p>
+        <Link
+          to="/AllProduct"
+          className="text-base md:text-lg bg-[#D4AF37] px-6 py-3 rounded-2xl hover:bg-[#a89c74]"
+        >
+          See All Product
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Components/SharedComponents/Navber/Navber.jsx
+++ src/Components/SharedComponents/Navber/Navber.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-[#ecebea] p-4 shadow-md">
+    <nav className="bg-[#ecebea] p-4 shadow-md sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center lg:justify-between">
         {/* Logo Section */}
         <div className="flex items-center">
